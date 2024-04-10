20b65e2565b3b936187a313b165502dcf17de8ba
aLadNamedPat <Pattheboy135@gmail.com>
2024-04-10 17:38:05 -0400

    Profile made

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, SafeAreaView } from "react-native";
+import Profile from "./screens/Profile";
 
 export default function App() {
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+  });
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
-    </View>
-  );
+    <SafeAreaView style={styles.container}>
+        <Profile/>
+    </SafeAreaView>
+    );
 }
